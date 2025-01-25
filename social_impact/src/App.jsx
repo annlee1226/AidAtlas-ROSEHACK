4d62f06fd61241c2afe4f83099a75099a7793333
sneha-grg <snehagrg14@gmail.com>
2025-01-25 15:23:26 -0800

    finish navbar

--- social_impact/src/App.jsx
+++ social_impact/src/App.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar.jsx';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
-  const[theme, setTheme] = useState('light');
+  const current_theme = localStorage.getItem('current_theme');
+  const[theme, setTheme] = useState(current_theme? current_theme : 'light');
+
+  useEffect(()=>{
+    localStorage.setItem('current_theme', theme);
+  },[theme])
 
   return (
     <div className={`container ${theme}`}>
